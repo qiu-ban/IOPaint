01947d871750d4049fbe40c3a8401fe6e56c6aa6
Aflytt <y190272204@163.com>
2024-06-18 21:41:37 +0800

    @dev

--- util.py
+++ util.py
@@ -1,60 +1,6 @@
 import base64
 from io import BytesIO
-import tos
 from PIL import Image
-from tos import HttpMethodType
-
-
-def put_image_2_tos(image_id, image_base64, content_type):
-    '''
-    上传图片到TOS
-    '''
-    try:
-        resp = tos_client.put_object(tos_bucket_name, image_id, content=BytesIO(base64.b64decode(image_base64)),
-                                     content_type=content_type)
-        return True
-    except tos.exceptions.TosClientError as e:
-        # 操作失败，捕获客户端异常，一般情况为非法请求参数或网络异常
-        print('fail with client error, message:{}, cause: {}'.format(e.message, e.cause))
-    except tos.exceptions.TosServerError as e:
-        # 操作失败，捕获服务端异常，可从返回信息中获取详细错误信息
-        print('fail with server error, code: {}'.format(e.code))
-        # request id 可定位具体问题，强烈建议日志中保存
-        print('error with request id: {}'.format(e.request_id))
-        print('error with message: {}'.format(e.message))
-        print('error with http code: {}'.format(e.status_code))
-        print('error with ec: {}'.format(e.ec))
-        print('error with request url: {}'.format(e.request_url))
-    except Exception as e:
-        print('fail with unknown error: {}'.format(e))
-
-
-def get_tos_access_url(image_id, expire_minutes=1):
-    return tos_client.pre_signed_url(HttpMethodType.Http_Method_Get, tos_bucket_name, image_id,
-                                                      expires=expire_minutes*60)
-
-
-def delete_image_2_oss(image_id):
-    '''
-    删除TOS的图片
-    '''
-    try:
-        resp = tos_client.delete_object(tos_bucket_name, image_id)
-        return True
-    except tos.exceptions.TosClientError as e:
-        # 操作失败，捕获客户端异常，一般情况为非法请求参数或网络异常
-        print('fail with client error, message:{}, cause: {}'.format(e.message, e.cause))
-    except tos.exceptions.TosServerError as e:
-        # 操作失败，捕获服务端异常，可从返回信息中获取详细错误信息
-        print('fail with server error, code: {}'.format(e.code))
-        # request id 可定位具体问题，强烈建议日志中保存
-        print('error with request id: {}'.format(e.request_id))
-        print('error with message: {}'.format(e.message))
-        print('error with http code: {}'.format(e.status_code))
-        print('error with ec: {}'.format(e.ec))
-        print('error with request url: {}'.format(e.request_url))
-    except Exception as e:
-        print('fail with unknown error: {}'.format(e))
 
 
 def compress_image(image_base64, format, target_size=1920):
